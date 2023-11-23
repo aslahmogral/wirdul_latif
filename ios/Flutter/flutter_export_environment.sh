16e47bbf19e8f64deb3b94f966e27c150818a856
aslahpillsbee <136675348+aslahpillsbee@users.noreply.github.com>
2023-11-23 20:32:43 +0530

    test pillsbee

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/aslahmogral/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/aslahmogral/Documents/wirdul_latif"
+export "FLUTTER_ROOT=C:\Users\aslah pillsbee\fvm\versions\3.16.0"
+export "FLUTTER_APPLICATION_PATH=C:\Users\aslah pillsbee\Documents\GitHub\wirdul_latif"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=0.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
